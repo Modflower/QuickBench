c4e4b78e1342cd6212edb685177ebfe656ffb932
unilock <unilock@fennet.rentals>
2024-02-01 10:54:40 -0500

    Add Unique annotation where missing
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/tfar/fastbench/mixin/CraftingContainerMixin.java
+++ src/main/java/tfar/fastbench/mixin/CraftingContainerMixin.java
@@ -33,6 +33,7 @@ import net.minecraft.world.inventory.TransientCraftingContainer;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import tfar.fastbench.interfaces.CraftingInventoryDuck;
 
@@ -42,6 +43,7 @@ public class CraftingContainerMixin implements CraftingInventoryDuck {
 	@Shadow
 	@Final
 	private AbstractContainerMenu menu;
+	@Unique
 	public boolean checkMatrixChanges = true;
 
 
